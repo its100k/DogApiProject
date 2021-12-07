0d48ae3b0bb96251cb71c0d976a8ec1944b560d4
HLudd <h.ludd89@gmail.com>
2021-12-06 19:30:49 -0800

    Commit project

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,17 +1,30 @@
 package com.example.myapplication
+import android.app.Application
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-
 import androidx.activity.viewModels
+import com.example.myapplication.database.DogEntity
+import com.example.myapplication.viewmodel.DogApplication
+import com.example.myapplication.viewmodel.MainActivity2
 import com.squareup.picasso.Picasso
 
-class MainActivity : AppCompatActivity() {
-    private val viewModel: MainViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    class MainActivity : AppCompatActivity() {
+        private val viewModel: MainViewModel by viewModels {
+            MainViewModel.DogViewModelFactory((application as DogApplication).database.dogDao())
+        }
+
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+   override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
 
 
@@ -25,6 +38,25 @@ class MainActivity : AppCompatActivity() {
 
         changeDogButton.setOnClickListener {
             viewModel.getNewDog()
+
+
+            val currentImgUrl = viewModel.dogEntity.value?.url
+            val newDogImage = currentImgUrl?.let { it1 ->
+                DogEntity(url = it1) }
+            if (newDogImage != null) {
+                viewModel.addDog(newDogImage)
+
+            }
+            val prevDogButton :Button = findViewById(R.id.button1)
+            prevDogButton.setOnClickListener {
+                val intent = Intent(this, MainActivity2::class.java)
+                startActivity(intent)
+            }
         }
     }
-}
+
+        }
+
+        }
+
+
